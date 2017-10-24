81ae11364c6b9c240a2e0d17dd5aaf50f920a73c
msd-rjoachim <33043645+msd-rjoachim@users.noreply.github.com>
2017-10-24 14:37:14 -0700

    Removed redundant auto-elevation code

--- Run-Executable.ps1
+++ Run-Executable.ps1
@@ -1,19 +1,3 @@
-<#
-#Elevates the script if it was not run from an Administrative Powershell instance.
-$MyFileName = "Run-Executable.ps1"
-$filebase = "$PSScriptRoot" + "\" + $MyFileName
-if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
-  Write-Output "Running as an Administrator."
-}
-else
-{
-  Write-Output "Restarting script as an Administrator"
-  Start-Process powershell -ArgumentList "-File $filebase" -verb RunAs
-  exit
-}
-#>
-
 #Set ExecutionPolicy for the duration of the script
 Set-ExecutionPolicy Bypass -Scope CurrentUser -Force
 
