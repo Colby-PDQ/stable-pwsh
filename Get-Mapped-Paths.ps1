18ca860c7d1b2ce2ae4cd9258a4ce159d75f1204
rjoachim <33043645+msd-rjoachim@users.noreply.github.com>
2017-10-27 09:41:32 -0700

    List all network drives
    List all network drives for the current user.

---
+++ Get-Mapped-Paths.ps1
@@ -0,0 +1,11 @@
+﻿#Displays paths for all network drives that are connected to the current user.
+
+&{
+$(
+    (get-psdrive -PSProvider FileSystem).
+        Where{$_.DisplayRoot -or $_.Root.StartsWith('\\')}.
+        ForEach{ '{0} - {1}' -f $_.Name, ($_.DisplayRoot, $_.Root)[!$_.DisplayRoot] } -join "`n"
+) 
+$PSCommandPath
+}
+Read-Host = ":"
