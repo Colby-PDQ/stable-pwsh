d417b528eef51e0dadc31f3ddf2b1735b1b82073
Ryan Joachim <33043645+msd-rjoachim@users.noreply.github.com>
2019-03-05 15:49:41 -0800

    Update AD-Filter-Admin-Comps.ps1
    Simplified and reorganized.

--- AD-Filter-Admin-Comps.ps1
+++ AD-Filter-Admin-Comps.ps1
@@ -2,14 +2,27 @@
 $ADMember = Get-ADGroupMember -Identity "All Administrators" -Recursive | Select-Object -ExpandProperty SamAccountName
 
 # Using the results from $ADMember, filter through all computer objects in AD that match the name in any way
-# and pull out the "sAMAccountName" ()
+# and pull out the "sAMAccountName"
 $ADComp = foreach ($member in $ADMember) {
-    Get-ADComputer -Filter "Name -like '*$member*'" | Select-Object -ExpandProperty sAMAccountName
+    Get-ADComputer -Filter "Name -like '*$member*'" -Properties Name, extensionAttribute1 | Select-Object -ExpandProperty sAMAccountName
 }
 
-foreach ($comp in $ADComp) {
-    # Add each computer in $ADComp to _All Admin Computers distribution group
-    Add-ADGroupMember -Identity "_All Admin Computers" -Members "$comp" -WhatIf
-    # Set the 'extensionAttribute1` attribute to "DND" (Do Not Disable)
-    Set-ADComputer -Identity "$comp" -Replace @{extensionAttribute1="DND"} -WhatIf
+$GroupMembers = Get-ADGroupMember -Identity '_All Admin Computers'
+$MissingComps = foreach ($Comp in $ADComp) {
+    if ($Comp -notin $GroupMembers.SamAccountName) {
+        $Comp
+        Write-Host -ForegroundColor Red "$comp will be added."
+        
     }
+    else {
+        Write-Host -ForegroundColor Green "$comp is already a member of the targeted group - skipping"
+    }
+}
+
+# Add each computer in $ADComp to _All Admin Computers distribution group
+Add-ADGroupMember -Identity "_All Admin Computers" -Members $MissingComps -WhatIf
+
+# Set the 'extensionAttribute1` attribute to "DND" (Do Not Disable)
+$MissingComps | Set-ADComputer -Replace @{extensionAttribute1 = "DND"} -WhatIf
+
+Read-Host "Press enter to exit"
