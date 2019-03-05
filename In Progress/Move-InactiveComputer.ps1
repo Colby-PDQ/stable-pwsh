0beced30c2ae7c44c7f9a8fb1a9b8bc39b78f6a0
Ryan Joachim <33043645+msd-rjoachim@users.noreply.github.com>
2019-03-04 17:29:27 -0800

    Create Move-InactiveComputer.ps1
    Find all computer objects that have not reported into AD for at least 45 days, plus additional filters for servers, specialized machines (based on naming scheme), only enabled machines, and that don't have a specific attribute value (example included).
    
    Results will be shown in a popup window for verification before user is prompted to proceed.

---
+++ In Progress/Move-InactiveComputer.ps1
@@ -0,0 +1,37 @@
+# Enable basic logging
+Start-Transcript -Path "C:\Logs\Move-Inactive.txt" -Force -NoClobber -Append
+Clear-Host
+
+# Gets time stamps for all computers in the domain that have NOT logged in since after specified date
+import-module activedirectory
+
+#Set the target time
+$DaysInactive = 45
+$time = (Get-Date).Adddays( - ($DaysInactive))
+ 
+# Get all AD computers with lastLogonTimestamp ($DaysInactive) less than the current day.
+# Example filters are included - change/add/remove as needed.
+$ADPull = Get-ADComputer -Filter {
+    LastLogonTimeStamp -lt $time -and
+    OperatingSystem -notlike '*server*' -and
+    Name -notlike '*ESX*' -and
+    Name -notlike '*server*' -and
+    Enabled -eq $True
+
+} -Properties LastLogonTimeStamp,extensionAttribute1 | Where-Object {$_.extensionAttribute1 -ne 'DND'} | Select-Object -ExpandProperty Name | Out-GridView
+
+$confirm = Read-Host -Prompt "Proceed? (y/n)"
+
+if ($confirm -eq "y") {
+    foreach ($comp in $ADPull) {
+        # Disable each computer before moving it.
+        Set-ADComputer -Identity $comp -Enabled $false -WhatIf
+        Move-ADObject -Identity $comp -TargetPath "OU=Computers,DC=contoso,DC=com" -WhatIf
+    }
+}
+
+Write-Host ""
+Read-Host -Prompt "Press any key to exit"
+
+#Stop logging
+Stop-Transcript
