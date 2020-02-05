38e34ff428de42d667586afc47b1fa86532c2d7f
Colby-PDQ <colby.bouma+github@pdq.com>
2020-02-05 12:49:45 -0700

    Update help section
    Fixed a couple typos and added the parameters.

--- Stable/PDQi_PSWindowsUpdate_CustomFields.ps1
+++ Stable/PDQi_PSWindowsUpdate_CustomFields.ps1
@@ -1,9 +1,15 @@
 <#
 .SYNOPSIS
-    Use the PSWindowsUpdate Powershell module to update 2 custom fields in PDQ Inventory (Last update check/Last update installed)
+    Use the PSWindowsUpdate PowerShell module to update 2 Custom Fields in PDQ Inventory (Last update check/Last update installed).
 .DESCRIPTION
     Update check and install times are not reported within Windows anywhere that is easily found. The closest option is the date(s) of the last installed hotfix, which is inaccurate at best.
-    Using the PSWindowsUpdate Powershell module, we are able to leverage it to update custom fields in Inventory - fields which can then be used as filters and reported on.
+    Using the PSWindowsUpdate PowerShell module, we are able to leverage it to update Custom Fields in Inventory - fields which can then be used as filters and reported on.
+.PARAMETER Days
+    The number of days since the last update check and last installed update that you want as your threshold.
+    Defaults to 30.
+.PARAMETER ServerHostName
+    The hostname of the computer that you have PDQ Inventory installed on. This script will use Invoke-Command to connect to the specificed hostname.
+    Defaults to "PDQSERVER".
 .EXAMPLE
     I have typically had this as a Tool in Inventory that I could run on-demand as needed (as well as remove possible conflicts with how Inventory handles running scripts vs how Deploy does it),
     but there is no reason you could not create a package in Deploy and schedule it if desired. Just be sure to test thoroughly.
@@ -17,7 +23,6 @@
     
     In this example the Custom Fields were created as a True/False (checkbox) type. If you want to use another type (integer for example),
     you'll need to change $Checked and $Installed to be set to your desired data type.
-
 #>
 
 [CmdletBinding()]
